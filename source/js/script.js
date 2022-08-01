04a2a2c0d3c617f4c9741f261c79c3069bb8e36e
Kvazim <mesvladimir@gmail.com>
2022-08-01 21:00:50 +0300

    кликабельная пагинация

--- source/js/script.js
+++ source/js/script.js
@@ -36,6 +36,8 @@ const swiper = new Swiper('.swiper', {
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
+    clickable: true,
+    clickableClass: 'swiper-pagination-clickable',
   },
 
   // Navigation arrows
