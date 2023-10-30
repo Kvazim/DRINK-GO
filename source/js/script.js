d9453e1b5e7168aa217b51cf31796a8c93cb49cf
Meshcheryakov Vladimir <89020510+Kvazim@users.noreply.github.com>
2023-10-30 15:40:39 +0300

    Update script.js

--- source/js/script.js
+++ source/js/script.js
@@ -15,6 +15,7 @@ const CENTER_MAP = {
 const LeafletParameters = {
   TILE_LAYER: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
   ATTRIBUTION: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  trackResize: true,
 };
 
 nav.classList.remove('navigation--nojs');
@@ -72,5 +73,7 @@ const marker = L.marker(
     icon: mainPinIcon,
   },
 );
+
+
 
 marker.addTo(map);
